0a5399c911a86343f20bc46f2b18894cb529b211
Zeal <zeal@ubuntuServer.(none)>
2013-10-02 21:19:43 +0600

    create\new works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,29 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+
+  def new
+    @work = Work.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @work = Work.new(work_params)
+
+    respond_to do |format|
+      if @work.save
+        format.html { redirect_to @work, notice: 'Work was successfully created.' }
+        format.json { render action: 'index', status: :created, location: @work }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @work.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
