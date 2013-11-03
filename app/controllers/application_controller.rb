859a1bb36c977236302d32e9a5ad94d150419389
Alexey <zeal@z-station.(none)>
2013-11-03 13:48:04 +0600

    close unauth pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate_user!, only: [:create, :destroy, :new, :update, :edit]
+
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
