19cd8baccc86cfb193ac2a9da8ddac51bf2c5968
Alexey Tataurov <zealav@gmail.com>
2014-04-02 00:09:17 +0600

    feature(work_types): update, destroy, output list

--- app/controllers/work_types_controller.rb
+++ app/controllers/work_types_controller.rb
@@ -11,15 +11,23 @@ class WorkTypesController < ApplicationController
     @work_type = WorkType.find(params[:id])
   end
 
+  def edit
+    @work_type = WorkType.find(params[:id])
+    @work_types = WorkType.all
+  end
+
+  def destroy
+    WorkType.find(params[:id]).destroy
+    redirect_to new_work_type_url
+  end
+
   def new
     @work_type = WorkType.new
-    @works_types = WorkType.all
+    @work_types = WorkType.all
   end
 
   def create
-    @work_type = WorkType.new
-    @work_type.name = params.require(:work_type).permit(:name)[:name]
-    @work_type.description = params.require(:work_type).permit(:description)[:description]
+    @work_type = WorkType.new(work_type_params)
 
     respond_to do |format|
       if @work_type.save
